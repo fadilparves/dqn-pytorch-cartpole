245c53966753d52ec48b056540b16eb7f8baa4f4
fadilparves <fadil.parves@gmail.com>
2020-10-03 23:06:47 +0800

    added replay memory class

--- dqn.py
+++ dqn.py
@@ -24,3 +24,23 @@ plt.ion()
 
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
 
+Transition = namedtuple('Transition', 
+                        ('state'. 'action', 'next_state', 'reward'))
+
+class ReplayMemory(object):
+    def __init__(self, capacity):
+        self.capacity = capacity
+        self.memory = []
+        self.position = 0
+    
+    def push(self, *args):
+        if len(self.memory) < self.capacity:
+            self.memory.append(None)
+        self.memory[self.position] = Transition(*args)
+        self.position = (self.position + 1) % self.capacity
+    
+    def sample(self, batch_size):
+        return random.sample(self.memory, batch_size)
+    
+    def __len__(self):
+        return len(self.memory)
